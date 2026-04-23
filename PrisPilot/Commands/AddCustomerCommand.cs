a63df5750f4c7cb97f4d8367d03fa389c3ba5032
Lauge Møller Andersen <46719124+LaugeM@users.noreply.github.com>
2026-04-23 13:32:43 +0200

    Added databinding to CustomerViewModel Added logo preview Added ability to convert chosen logo to bytearray Added savebutton Added preview of users input Added logic to Add method of CustomerRepository Added custom fonts to assets (not working properly)

---
+++ PrisPilot/Commands/AddCustomerCommand.cs
@@ -0,0 +1,52 @@
+﻿using PrisPilot.Services.Interfaces;
+using PrisPilot.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace PrisPilot.Commands
+{
+    public class AddCustomerCommand : ICommand
+    {
+        
+        public AddCustomerCommand()
+        {
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            if (parameter is AddCustomerViewModel ACM)
+            {
+                var c = ACM.CurrentCustomer;
+                string message =
+                    $"Firmanavn: {c.CompanyName}{Environment.NewLine}" +
+                    $"CVR: {c.Cvr}{Environment.NewLine}" +
+                    $"Email: {c.Email}{Environment.NewLine}" +
+                    $"Telefonnummer: {c.TelephoneNumber}{Environment.NewLine}" +
+                    $"adresse: {c.Address}{Environment.NewLine}" +
+                    $"Kontaktperson: {c.ContactPerson}{Environment.NewLine}" +
+                    $"Timepris: {c.HourlyCost}{Environment.NewLine}" +
+                    $"Logo: {ACM.SelectedFilePath}";
+
+                MessageBox.Show(message, "Indtastede oplysninger");
+            }
+            else
+            {
+                throw new ArgumentException("Temporary error");
+            }
+        }
+    }
+}
--- PrisPilot/ViewModels/CustomerViewModel.cs
+++ PrisPilot/ViewModels/CustomerViewModel.cs
@@ -2,13 +2,15 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Text;
+using System.Windows.Media.Imaging;
 
 namespace PrisPilot.ViewModels
 {
     public class CustomerViewModel : BaseViewModel<Customer>
     {
-        private Customer customer;
+        private Customer _customer;
 
         private string _companyName = string.Empty;
         public string CompanyName
@@ -70,14 +72,29 @@ namespace PrisPilot.ViewModels
             }
         }
 
-        private Image _logo;
-        public Image Logo
+        private byte[]? _logo;
+        public byte[]? Logo
         {
             get => _logo;
             set
             {
                 if (_logo == value) return;
                 _logo = value;
+                OnPropertyChanged();
+                UpdatePreviewFromLogo();
+            }
+        }
+
+        // This is for our WPF preview
+        // This is placed inside this class to help us access it from anywhere we're using the CustomerViewModel
+        private BitmapImage? _previewImage;
+        public BitmapImage? PreviewImage
+        {
+            get => _previewImage;
+            private set
+            {
+                if (_previewImage == value) return;
+                _previewImage = value;
                 OnPropertyChanged();
             }
         }
@@ -108,8 +125,42 @@ namespace PrisPilot.ViewModels
 
         public CustomerViewModel(Customer customer) : base(customer)
         {
-            this.customer = customer;
-            
+            this._customer = customer;
+            this.CompanyName = customer.CompanyName;
+            this.Cvr = customer.Cvr;
+            this.Email = customer.Email;
+            this.TelephoneNumber = customer.TelephoneNumber;
+            this.Address = customer.Address;
+            this.Logo = customer.Logo;
+            this.ContactPerson = customer.ContactPerson;
+            this.HourlyCost = customer.HourlyCost;
+        }
+
+        private void UpdatePreviewFromLogo()
+        {
+            // Check if logo exists, returns null if it doesn't
+            if (_logo == null)
+            {
+                PreviewImage = null;
+                return;
+            }
+
+            // Creating a BitmapImage from our bytearray
+            try
+            {
+                using MemoryStream ms = new MemoryStream(_logo);
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.StreamSource = ms;
+                bitmap.EndInit();
+                bitmap.Freeze();
+                PreviewImage = bitmap;
+            }
+            catch
+            {
+                PreviewImage = null;
+            }
         }
     }
 }
