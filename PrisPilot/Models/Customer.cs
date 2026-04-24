f412a36ae1d335351cb2ddd2424ccda20c52cc98
Lauge Møller Andersen <46719124+LaugeM@users.noreply.github.com>
2026-04-24 11:39:04 +0200

    Started switch to strings

--- PrisPilot/Models/Customer.cs
+++ PrisPilot/Models/Customer.cs
@@ -8,9 +8,9 @@ namespace PrisPilot.Models
     public class Customer
     {
         public string CompanyName { get; set; } = string.Empty;
-        public int Cvr { get; set; } = 0;
+        public string Cvr { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public int TelephoneNumber { get; set; } = 0;
+        public string TelephoneNumber { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
         // We're storing the logo as a byte array, since storing it as an image file was a PITA.
         // We're also going to add it to our database as a bytearray anyways
