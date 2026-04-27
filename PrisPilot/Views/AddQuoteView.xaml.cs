fdcfe621cdf9cb229d732886bd5706fe37580be8
Amin Christiansen <Amin081204@gmail.com>
2026-04-27 09:21:14 +0200

    quote view

---
+++ PrisPilot/Views/AddQuoteView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using PrisPilot.Services;
+using PrisPilot.ViewModels;
+using PrisPilot.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrisPilot.Views
+{
+    /// <summary>
+    /// Interaction logic for AddCustomerView.xaml
+    /// </summary>
+    public partial class AddCustomerView : UserControl
+    {
+        private readonly IFileDialogService _fileDialogService;
+        private readonly AddCustomerViewModel ACM;
+
+        public AddCustomerView()
+        {
+            InitializeComponent();
+            _fileDialogService = new FileDialogService();
+            ACM = new AddCustomerViewModel(_fileDialogService);
+            DataContext = ACM;
+        }
+    }
+}
