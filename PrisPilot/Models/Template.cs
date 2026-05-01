bc8c6859b04f23ad48de0922d53d00429491589c
Nadia Jensen <Nadiajjensen98@hotmail.com>
2026-05-01 10:50:10 +0200

    Classes med tilhørende atributter

---
+++ PrisPilot/Models/Template.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrisPilot.Models
+{
+    public class Template
+    {
+        public string Introduction { get; set; }
+        public string TimeTable { get; set; }
+        public bool AboutUs { get; set; }
+        public string Title { get; set; }
+        public string DrosselMail { get; set; }
+        public string DrosselPhoneNumber { get; set; }
+        public byte[] DrosselLogo { get; set; }
+
+
+    }
+}
