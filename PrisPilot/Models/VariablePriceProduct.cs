6dcfd7301130301db62083c8d02c56984bca22d7
Lauge Møller Andersen <46719124+LaugeM@users.noreply.github.com>
2026-05-04 13:31:25 +0200

    Add variablepriceproduct repo

--- PrisPilot/Models/VariablePriceProduct.cs
+++ PrisPilot/Models/VariablePriceProduct.cs
@@ -6,8 +6,8 @@ namespace PrisPilot.Models
 {
     public class VariablePriceProduct
     {
+        public int VariablePriceProductID { get; set; }
         public string Name { get; set; }
-
         public string Description { get; set; }
     }
 }
---
+++ PrisPilot/Services/Peristence/VariablePriceProductRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Data.SqlClient;
+using PrisPilot.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace PrisPilot.Services.Peristence
+{
+    public class VariablePriceProductRepository : BaseRepository<VariablePriceProduct>
+    {
+        public VariablePriceProductRepository() : base()
+        {
+        }
+
+        public override VariablePriceProduct Add(VariablePriceProduct variablePriceProduct)
+        {
+            using (SqlConnection con = CreateConnection())
+            {
+                con.Open();
+
+                using SqlCommand cmd = new SqlCommand(@"
+                            INSERT INTO CUSTOMER (Name, Description)
+                            VALUES (@Name, @Description)" + "SELECT @@IDENTITY", con);
+
+                cmd.Parameters.Add("@Name", SqlDbType.NVarChar, 80).Value = variablePriceProduct.Name;
+                cmd.Parameters.Add("@Description", SqlDbType.NVarChar, 1200).Value = variablePriceProduct.Description;
+                variablePriceProduct.VariablePriceProductID = Convert.ToInt32(cmd.ExecuteScalar());
+            }
+            return variablePriceProduct;
+        }
+
+        public override List<VariablePriceProduct> GetAll()
+        {
+            List<VariablePriceProduct> variablePriceProducts = new List<VariablePriceProduct>();
+            using (SqlConnection con = CreateConnection())
+            {
+                con.Open();
+                using SqlCommand cmd = new SqlCommand("SELECT VariablePriceProduct, Name, Description FROM VariablePriceProduct", con);
+                using SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    VariablePriceProduct variablePriceProduct = new VariablePriceProduct
+                    {
+                        VariablePriceProductID = reader.GetInt32(0),
+                        Name = reader["Name"] != DBNull.Value ? reader["Name"].ToString()! : string.Empty,
+                        Description = reader["Description"] != DBNull.Value ? reader["Description"].ToString()! : string.Empty
+                    };
+                    variablePriceProducts.Add(variablePriceProduct);
+                }
+            }
+            return variablePriceProducts;
+        }
+    }
+}
