298847caae115b54847f3fa5ac70f2cb2ad5f8e0
Lauge Møller Andersen <46719124+LaugeM@users.noreply.github.com>
2026-05-04 13:00:02 +0200

    Added ability to add linking table and TimeSpent to database

--- PrisPilot/Services/Peristence/QuoteRepository.cs
+++ PrisPilot/Services/Peristence/QuoteRepository.cs
@@ -19,9 +19,10 @@ namespace PrisPilot.Services.Peristence
             {
                 con.Open();
 
-                using SqlCommand Cmd = new SqlCommand(@" INSERT INTO QUOTE (Date, HourlyCost, TotalPrice)"
-                                                               + "VALUES (@Date, @HourlyCost, @TotalPrice)"
-                                                               + "SELECT @@IDENTITY", con);
+                using SqlCommand Cmd = new SqlCommand(@"
+                        INSERT INTO QUOTE (Date, HourlyCost, TotalPrice)"
+                        + "VALUES (@Date, @HourlyCost, @TotalPrice)"
+                        + "SELECT @@IDENTITY", con);
 
                 Cmd.Parameters.Add("@Date", SqlDbType.DateTime2).Value = quote.Date;
                 Cmd.Parameters.Add("@HourlyCost", SqlDbType.Int).Value = quote.HourlyCost;
@@ -56,6 +57,66 @@ namespace PrisPilot.Services.Peristence
             }
 
             return quotes;
+        }
+
+        // Link a FixedPriceProduct to a Quote
+        public void AddFixedPriceProductToQuote(int quoteId, int fixedPriceProductId)
+        {
+            using (SqlConnection con = CreateConnection())
+            {
+                con.Open();
+                using SqlCommand cmd = new SqlCommand(
+                    "INSERT INTO FIXEDPRICEPRODUCT_QUOTE (FixedPriceProductID," +
+                    "QuoteID) VALUES (@FixedPriceProductID, @QuoteID)", con);
+                cmd.Parameters.Add("@FixedPriceProductID", SqlDbType.Int).Value = fixedPriceProductId;
+                cmd.Parameters.Add("@QuoteID", SqlDbType.Int).Value = quoteId;
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        // Remove link between Quote and FixedPriceProduct
+        public void RemoveFixedPriceProductFromQuote(int quoteId, int fixedPriceProductId)
+        {
+            using (SqlConnection con = CreateConnection())
+            {
+                con.Open();
+                using SqlCommand cmd = new SqlCommand(
+                    "DELETE FROM FIXEDPRICEPRODUCT_QUOTE" +
+                    "WHERE FixedPriceProductID = @FixedPriceProductID AND QuoteID = @QuoteID", con);
+                cmd.Parameters.Add("@FixedPriceProductID", SqlDbType.Int).Value = fixedPriceProductId;
+                cmd.Parameters.Add("@QuoteID", SqlDbType.Int).Value = quoteId;
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        // Get all FixedPriceProduct associated to a Quote
+        public List<FixedPriceProduct> GetFixedPriceProductsForQuote(int quoteId)
+        {
+            List<FixedPriceProduct> results = new List<FixedPriceProduct>();
+            using (SqlConnection con = CreateConnection())
+            {
+                con.Open();
+                using SqlCommand cmd = new SqlCommand(@"
+                    SELECT fp.FixedPriceProductID, fp.Name, fp.Description, fp.Price, fp.Frequency
+                    FROM FIXEDPRICEPRODUCT fp
+                    JOIN FIXEDPRICEPRODUCT_QUOTE fq ON fp.FixedPriceProductID = fq.FixedPriceProductID
+                    WHERE fq.QuoteID = @QuoteID", con);
+                cmd.Parameters.Add("@QuoteID", SqlDbType.Int).Value = quoteId;
+                using SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    FixedPriceProduct fp = new FixedPriceProduct
+                    {
+                        FixedPriceProductID = reader.GetInt32(0),
+                        Name = reader["Name"] != DBNull.Value ? reader["Name"].ToString()! : string.Empty,
+                        Description = reader["Description"] != DBNull.Value ? reader["Description"].ToString()! : string.Empty,
+                        Price = reader["Price"] != DBNull.Value ? Convert.ToInt32(reader["Price"]) : 0,
+                        Frequency = reader["Frequency"] != DBNull.Value ? Convert.ToInt32(reader["Frequency"]) : 0
+                    };
+                    results.Add(fp);
+                }
+            }
+            return results;
         }
 
     }
