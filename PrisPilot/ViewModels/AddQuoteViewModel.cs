3f92f13d8699b9f3c1518a38b9014cdeb32928e9
Lauge Møller Andersen <46719124+LaugeM@users.noreply.github.com>
2026-05-05 12:27:55 +0200

    Began work on adding dynamic product selection to quote

--- PrisPilot/ViewModels/AddQuoteViewModel.cs
+++ PrisPilot/ViewModels/AddQuoteViewModel.cs
@@ -33,10 +33,83 @@ namespace PrisPilot.ViewModels
             }
         }
 
+        private bool _showLinkedInFastVaretagelse;
+        public bool ShowLinkedInFastVaretagelse
+        {
+            get => _showLinkedInFastVaretagelse;
+            set
+            {
+                if (_showLinkedInFastVaretagelse == value) return;
+                _showLinkedInFastVaretagelse = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(SelectedProductTypesText));
+            }
+        }
+
+        private bool _showFastvaretagelseKommunikation;
+        public bool ShowFastvaretagelseKommunikation
+        {
+            get => _showFastvaretagelseKommunikation;
+            set
+            {
+                if (_showFastvaretagelseKommunikation == value) return;
+                _showFastvaretagelseKommunikation = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(SelectedProductTypesText));
+            }
+        }
+
+        private bool _showSalgsOgPraesentationMateriale;
+        public bool ShowSalgsOgPraesentationMateriale
+        {
+            get => _showSalgsOgPraesentationMateriale;
+            set
+            {
+                if (_showSalgsOgPraesentationMateriale == value) return;
+                _showSalgsOgPraesentationMateriale = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(SelectedProductTypesText));
+            }
+        }
+
+        private bool _showYderligereTilfoejelser;
+        public bool ShowYderligereTilfoejelser
+        {
+            get => _showYderligereTilfoejelser;
+            set
+            {
+                if (_showYderligereTilfoejelser == value) return;
+                _showYderligereTilfoejelser = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(SelectedProductTypesText));
+            }
+        }
+
+        public string SelectedProductTypesText
+        {
+            get
+            {
+                List<string> selected = new List<string>();
+
+                if (ShowLinkedInFastVaretagelse) selected.Add("LinkedIn");
+                if (ShowFastvaretagelseKommunikation) selected.Add("Kommunikation");
+                if (ShowSalgsOgPraesentationMateriale) selected.Add("Salgs/præsentation");
+                if (ShowYderligereTilfoejelser) selected.Add("Yderligere");
+
+                return selected.Count == 0 ? "Vælg produkttyper..." : string.Join(", ", selected);
+            }
+        }
+
         public AddQuoteViewModel()
         {
             _customerRepository = new CustomerRepository();
             LoadCustomers();
+
+            // Default: show nothing until the user chooses in the dropdown
+            ShowLinkedInFastVaretagelse = false;
+            ShowFastvaretagelseKommunikation = false;
+            ShowSalgsOgPraesentationMateriale = false;
+            ShowYderligereTilfoejelser = false;
         }
 
         private void LoadCustomers()
