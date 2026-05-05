a93e166340b445ec42d73722ca5e77209d86a12f
Amin Christiansen <Amin081204@gmail.com>
2026-05-05 12:43:32 +0200

    pdf getting regenerated, and shown but wont opdate because of databinding

--- PrisPilot/ViewModels/AddQuoteViewModel.cs
+++ PrisPilot/ViewModels/AddQuoteViewModel.cs
@@ -1,23 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Text;
-using PrisPilot.Models;
+﻿using PrisPilot.Models;
+using PrisPilot.Services;
 using PrisPilot.Services.Peristence;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
 
 namespace PrisPilot.ViewModels
 {
     public class AddQuoteViewModel : SuperClassViewModel
     {
         private readonly CustomerRepository _customerRepository;
+        private readonly QuoteRepository _quoteRepository;
 
-        private ObservableCollection<Customer> _customers = new ObservableCollection<Customer>();
-        public ObservableCollection<Customer> Customers
+        public ObservableCollection<Customer> Customers { get; set; }
+
+        public QuoteDraft Draft { get; } = new();
+
+        private Uri? _pdfPreviewUri;
+        public Uri? PdfPreviewUri
         {
-            get => _customers;
+            get => _pdfPreviewUri;
             set
             {
-                _customers = value;
+                _pdfPreviewUri = value;
                 OnPropertyChanged();
             }
         }
@@ -29,22 +35,59 @@ namespace PrisPilot.ViewModels
             set
             {
                 _selectedCustomer = value;
+                Draft.Customer = value;
                 OnPropertyChanged();
+                RegeneratePreview();
             }
         }
 
         public AddQuoteViewModel()
         {
             _customerRepository = new CustomerRepository();
+            _quoteRepository = new QuoteRepository();
             LoadCustomers();
         }
 
         private void LoadCustomers()
         {
-            var customers = _customerRepository.GetAll();
-            Customers = new ObservableCollection<Customer>(customers);
+            Customers = new ObservableCollection<Customer>(
+                _customerRepository.GetAll());
         }
 
+        private void RegeneratePreview()
+        {
+            Debug.WriteLine("REGENERATE PREVIEW CALLED " + DateTime.Now);
+            Draft.Subtotal = Draft.FixedPriceProducts.Sum(p => p.Price);
 
+            string path = Path.Combine(
+                Path.GetTempPath(),
+                "quote_preview.pdf");
+
+            QuotePdfGenerator.GeneratePreview(path, Draft);
+            PdfPreviewUri = new Uri(path);
+        }
+
+        public void SaveQuote()
+        {
+            var quote = new Quote
+            {
+                Date = DateTime.Now,
+                TotalPrice = Draft.Total
+            };
+
+            quote = _quoteRepository.Add(quote);
+
+            foreach (var product in Draft.FixedPriceProducts)
+            {
+                _quoteRepository.AddFixedPriceProductToQuote(
+                    quote.QuoteID,
+                    product.FixedPriceProductID);
+            }
+
+            var finalPath =
+                $@"C:\Tilbud\Tilbud_{quote.QuoteID}.pdf";
+
+            QuotePdfGenerator.GenerateFinal(finalPath, Draft, quote);
+        }
     }
 }
