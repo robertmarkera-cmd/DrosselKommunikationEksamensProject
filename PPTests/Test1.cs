a9188093ba76763002b5b501cd82c115ee72cdab
Lauge Møller Andersen <46719124+LaugeM@users.noreply.github.com>
2026-05-08 12:01:17 +0200

    Initial commit for unittest

---
+++ PPTests/Test1.cs
@@ -0,0 +1,32 @@
+﻿using PrisPilot;
+using PrisPilot.Models;
+using PrisPilot.ViewModels;
+
+namespace PPTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        AddQuoteViewModel addQuoteViewModel;
+
+        [TestInitialize]
+        public void Init()
+        {
+            // Instantiating VariablePriceProducts
+            VariablePriceProduct vp1 = new();
+            VariablePriceProduct fp1 = new();
+
+            // Initializing
+            
+
+            // Arrange
+            addQuoteViewModel = new();
+            //addQuoteViewModel.SelectedProducts.Add();
+        }
+
+        [TestMethod]
+        public void CreateCurrentDraft_WithMixedProducts_CalculatesSubtotalCorrectly()
+        {
+        }
+    }
+}
